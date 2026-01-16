c3fe9b9faee81c2489d262a2c5f7f76990d1036b
Chú bé đần <dokhangvu@gmail.com>
2026-01-17 00:30:15 +0700

    Update MazeSolver.h

--- MazeProject/solver/MazeSolver.h
+++ MazeProject/solver/MazeSolver.h
@@ -1,1 +1,15 @@
+#ifndef MAZE_SOLVER_H
+#define MAZE_SOLVER_H
 
+#include "../core/Maze.h"
+#include <vector>
+
+using namespace std;
+
+class MazeSolver {
+public:
+    static bool solveDFS(Maze& maze, int sx, int sy, int ex, int ey);
+    static bool solveBFS(Maze& maze, int sx, int sy, int ex, int ey);
+};
+
+#endif
