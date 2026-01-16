41fa422ed3f9383d70f55b28f07b83e9afb8a924
DuyThuan1205 <thuan21026@gmail.com>
2026-01-16 22:13:45 +0700

    Add files via upload

---
+++ TaoMeCung.cpp
@@ -0,0 +1,63 @@
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <algorithm>
+
+using namespace std;
+
+const int H = 15;
+const int W = 31;
+
+char maze[H][W];
+
+// 4 hướng: phải, trái, xuống, lên
+int dx[4] = {0, 0, 2, -2};
+int dy[4] = {2, -2, 0, 0};
+
+// Kiểm tra ô có hợp lệ để đi tiếp không
+bool valid(int x, int y) {
+    return x > 0 && x < H - 1 &&
+           y > 0 && y < W - 1 &&
+           maze[x][y] == '#';
+}
+
+// Sinh mê cung bằng DFS Backtracking
+void generateMaze(int x, int y) {
+    maze[x][y] = ' ';
+
+    int dir[4] = {0, 1, 2, 3};
+    random_shuffle(dir, dir + 4);
+
+    for (int i = 0; i < 4; i++) {
+        int nx = x + dx[dir[i]];
+        int ny = y + dy[dir[i]];
+
+        if (valid(nx, ny)) {
+            // Phá tường ở giữa
+            maze[x + dx[dir[i]] / 2][y + dy[dir[i]] / 2] = ' ';
+            generateMaze(nx, ny);
+        }
+    }
+}
+
+int main() {
+    srand(time(NULL));
+
+    // Khởi tạo mê cung toàn tường
+    for (int i = 0; i < H; i++)
+        for (int j = 0; j < W; j++)
+            maze[i][j] = '#';
+
+    // Bắt đầu sinh mê cung từ (1,1)
+    generateMaze(1, 1);
+
+    // In mê cung
+    for (int i = 0; i < H; i++) {
+        for (int j = 0; j < W; j++)
+            cout << maze[i][j];
+        cout << endl;
+    }
+
+    return 0;
+}
